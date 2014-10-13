8d757e86863bbf99123fcc0eefc022c4db7fac15
xuelangZF <xuezaigds@gmail.com>
2014-10-13 14:10:37 +0800

    simple html2dash program

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-html2Dash
-=========
+# html2Dash
 
-Generate a docset from any HTML documentation and add it to Dash
+html2Dash is an Documentation Set generator intended to be used with the [Dash.app](http://kapeli.com/dash/) API browser for OS X or one of its many clones. html2Dash is just like [doc2dash](https://github.com/hynek/doc2dash) but generating docset from any HTML documentations.
+
+If you’ve never heard of Dash.app, you’re missing out: together with html2Dash it’s all your API documentation at your fingertips!
+
+It’s tested on Python 2.7, OS X 10.9
+
+# How to Use
+
+The usage is as simple as:
+
+	$ html2Dash <htmldir>
+
+html2dash will create a new directory called `<htmldir>.docset` in `~/Library/Application Support/html2dash/DocSets` containing a Dash.app-compatible docset. When finished, the docset is automatically added to Dash.app.
+
+**Options and Arguments**
+
+The full usage is:
+
+	$ doc2dash [OPTIONS] SOURCE  
+
+The `SOURCE` is a directory containing the HTML documents you would like to convert.
+
+Valid `OPTIONS` are the following:
+
+* -n, --name  
+
+	Name the docset explicitly instead of letting doc2dash guess the correct name from the directory name of the source.
+
+* -d PATH, --destination PATH  
+
+	Put the resulting docset into PATH. Default is the directory `~/Library/Application Support/html2dash/DocSets` 
+
+* -i FILENAME, --icon FILENAME
+
+	Add PNG icon FILENAME to docset that is used within Dash.app to represent the docset.
+	
+* -p INDEX_PAGE, --index-page INDEX_PAGE
+	
+	Set the file that is shown when the docset is clicked within Dash.app.
+	
+* -h, --help
+
+	Show a brief usage summary and exit.
+
+# Demo
+
+1. 生成bs4的文档`beautifulsoup4.docset`，命令如下：
+
+	$ ./html2dash.py -n requests -i ~/Documents/requests-sidebar.png ~/Documents/requests  
+	Create the Docset Folder!  
+	Copy the HTML Documentation!  
+	Create the SQLite Index    
+	Create the Info.plist File  
+	Create the Icon for the Docset!  
+	Generate Docset Successfully!  
---
+++ html2dash.py
@@ -0,0 +1,193 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import re
+import sqlite3
+import subprocess
+from bs4 import BeautifulSoup
+
+
+def update_db(name, path):
+    try:
+        cur.execute("SELECT rowid FROM searchIndex WHERE path = ?", (path,))
+        dbpath = cur.fetchone()
+        cur.execute("SELECT rowid FROM searchIndex WHERE name = ?", (name,))
+        dbname = cur.fetchone()
+
+        if dbpath is None and dbname is None:
+            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path)\
+                    VALUES (?,?,?)', (name, "Section", path))
+        else:
+            pass
+    except:
+        pass
+
+
+def add_urls():
+    index_page = open(os.path.join(docset_path, 'index.html')).read()
+    soup = BeautifulSoup(index_page)
+    any = re.compile('.*')
+    for tag in soup.find_all('a', {'href': any}):
+        name = tag.text.strip()
+        if len(name) > 0:
+            path = tag.attrs['href'].strip()
+            if path.split('#')[0] not in ('index.html'):
+                update_db(name, path)
+
+
+def add_infoplist(info_path, index_page):
+    name = docset_name.split('.')[0]
+    info = """
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+            <key>CFBundleIdentifier</key>
+            <string>{0}</string>
+            <key>CFBundleName</key>
+            <string>{1}</string>
+            <key>DashDocSetFamily</key>
+            <string>{2}</string>
+            <key>DocSetPlatformFamily</key>
+            <string>requests</string>
+            <key>isDashDocset</key>
+            <true/>
+            <key>isJavaScriptEnabled</key>
+            <true/>
+            <key>dashIndexFilePath</key>
+            <string>{3}</string>
+    </dict>
+    </plist>
+    """.format(name, name, name, index_page)
+
+    try:
+        open(info_path, 'wb').write(info)
+        print "Create the Info.plist File"
+    except:
+        print "**Error**:  Create the Info.plist File Failed..."
+        clear_trash()
+        exit(2)
+
+
+def clear_trash():
+    try:
+        subprocess.call(["rm", "-r", docset_name])
+        print "Clear generated useless files!"
+    except:
+        print "**Error**:  Clear trash failed..."
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-n', '--name',
+                        help='Name the docset explicitly')
+    parser.add_argument('-d', '--destination',
+                        dest='path',
+                        default='',
+                        help='Put the resulting docset into PATH')
+    parser.add_argument('-i', '--icon',
+                        dest='filename',
+                        help='Add PNG icon FILENAME to docset')
+    parser.add_argument('-p', '--index-page',
+                        help='Set the file that is shown')
+    parser.add_argument('SOURCE',
+                        help='Directory containing the HTML documents')
+
+    results = parser.parse_args()
+
+    source_dir = results.SOURCE
+    if source_dir[-1] == "/":
+        source_dir = results.SOURCE[:-1]
+
+    if not os.path.exists(source_dir):
+        print source_dir + " does not exsit!"
+        exit(2)
+
+    dir_name = os.path.basename(source_dir)
+    if not results.name:
+        docset_name = dir_name + ".docset"
+    else:
+        docset_name = results.name + ".docset"
+
+    # create docset directory and copy files
+    doc_path = docset_name + "/Contents/Resources/Documents"
+    dsidx_path = docset_name + "/Contents/Resources/docSet.dsidx"
+    icon_path = docset_name + "/icon.png"
+    info = docset_name + "/Contents/info.plist"
+    docset_path = results.path + doc_path
+    sqlite_path = results.path + dsidx_path
+    info_path = results.path + info
+
+    # print docset_path, sqlite_path
+
+    if not os.path.exists(docset_path):
+        os.makedirs(docset_path)
+        print "Create the Docset Folder!"
+    else:
+        print "Docset Folder already exist!"
+
+    # Copy the HTML Documentation to the Docset Folder
+    try:
+        subprocess.call(["cp", "-r", results.SOURCE + "/", docset_path])
+        print "Copy the HTML Documentation!"
+    except:
+        print "**Error**:  Copy Html Documents Failed..."
+        clear_trash()
+        exit(2)
+
+    # create and connect to SQLite
+    try:
+        db = sqlite3.connect(sqlite_path)
+        cur = db.cursor()
+    except:
+        print "**Error**:  Create SQLite Index Failed..."
+        clear_trash()
+        exit(2)
+
+    try:
+        cur.execute('DROP TABLE searchIndex;')
+    except:
+        pass
+
+    cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY,\
+                name TEXT,\
+                type TEXT,\
+                path TEXT);')
+    cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+    print "Create the SQLite Index"
+
+    add_urls()
+    db.commit()
+    db.close()
+
+    # Create the Info.plist File
+    if not results.index_page:
+        index_page = "index.html"
+    else:
+        index_page = results.index_page
+
+    add_infoplist(info_path, index_page)
+
+    # Add icon file if defined
+    icon_filename = results.filename
+    if icon_filename:
+        if icon_filename[-4:] == ".png" and os.path.isfile(icon_filename):
+            try:
+                subprocess.call(["cp", icon_filename, icon_path])
+                print "Create the Icon for the Docset!"
+            except:
+                print "**Error**:  Copy Icon file failed..."
+                clear_trash()
+                exit(2)
+        else:
+            print "**Error**:  Icon file should be a valid PNG image..."
+            clear_trash()
+            exit(2)
+    else:
+        pass
+
+    print "Generate Docset Successfully!"
